029eae0efd597f58987cb06dc1f40fb950728598
Rich Groth <richardcgroth@gmail.com>
2026-05-22 20:36:27 -0400

    first commit

---
+++ assets/js/_vti_cnf/roll.js
@@ -0,0 +1,7 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|Maria Bochkova
+vti_timecreated:TR|21 May 2004 13:02:50 -0400
+vti_modifiedby:SR|Maria Bochkova
+vti_backlinkinfo:VX|directions.html contact.html index.html
+vti_extenderversion:SR|3.0.2.926
+vti_timelastmodified:TR|21 May 2004 13:02:50 -0400
